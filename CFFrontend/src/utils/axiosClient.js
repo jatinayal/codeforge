a382e3262e3629b80b60df19aadb158df175676e
jatinayal <jatinsingh89234@gmail.com>
2025-09-14 20:27:37 +0530

    frontend ready

--- CFFrontend/src/utils/axiosClient.js
+++ CFFrontend/src/utils/axiosClient.js
@@ -1,13 +1,11 @@
 import axios from 'axios'
 
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
-
 const axiosClient = axios.create({
-    baseURL: baseURL,
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default axiosClient;
